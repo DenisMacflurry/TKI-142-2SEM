aaf106b8e29ee64e2e3a106cadec5ca1ee2dabc6
Денис Ковтун <bdjibm@bk.ru>
2025-06-02 10:12:27 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -2,35 +2,21 @@
 
 Point::Point(double x, double y, double z) : x(x), y(y), z(z) {}
 
+double Point::getX() const { return x; }
+double Point::getY() const { return y; }
+double Point::getZ() const { return z; }
 
-double Point::getX() const 
-{ 
-    return x;
-}
-
-double Point::getY() const 
-{ 
-    return y;
-}
-
-double Point::getZ() const
-{ 
-    return z;
-}
-
-double Point::calculating_the_distance(const Point &other) const
-{ 
+double Point::calculating_the_distance(const Point &other) const {
     double dx = x - other.x;
     double dy = y - other.y;
     double dz = z - other.z;
+    return sqrt(dx*dx + dy*dy + dz*dz);
+}
 
-    double distance = (pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
+bool Point::operator==(const Point& other) const {
+    return x == other.x && y == other.y && z == other.z;
+}
 
-    if (distance < 0)
-    { 
-        std::cerr << "Имеем ошибочку, отрицательное расстрояние между точками, такого быть не может\n";
-        exit(1);
-    }
-
-    return sqrt(distance);
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);
 }
